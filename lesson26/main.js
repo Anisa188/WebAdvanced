6a8d3fd376dbe04fecc70c213bc0ff234e8ddf17
Anisa188 <anisathaqi85@gmail.com>
2026-03-30 16:14:57 +0200

    commit

--- lesson26/main.js
+++ lesson26/main.js
@@ -1,8 +1,8 @@
-console.log("Goodbye")
+console.log("Goodbye");
 
-var x = 5;
+//var x = 5;
 
-var a = 50 + 50
+var a = 50 + 50;
 console.log(a);
 
 var b = 20;
@@ -18,4 +18,67 @@ console.log(g);
 var h = 10;
 var i = 4;
 var  j = h % i;
-console.log(j)
+console.log(j);
+
+var k =99 ;
+k++;
+console.log(k);
+
+var l =113 ;
+l--;
+console.log(l);
+
+var p = 20;
+var q = p**2;
+console.log(q);
+
+var r = 15;
+r=+10;
+console.log(r);
+
+var r = 15;
+r=-10;
+console.log(r);
+
+var x = 20;
+x*=5;
+console.log(x);
+
+var x = 20;
+x/=5;
+console.log(x);
+
+var s = 5;
+var t = 5;
+console.log(s ==t);
+
+var y = 5;
+var u  = 6;
+console.log(y != u);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 < num2);
+
+var num3 = 2;
+var num4 = 1;
+console.log(num3 > num4);
+
+var num5 = 123;
+var num6 = "123";
+console.log(num5 === num6);
+
+var firstName = "Digital";
+var lastName = "school";
+var  fullName  = firstName + " "+ lastName;
+console.log(fullName);
+
+var school = "Digital school";
+console.log(`Hello from ${school}`);
+
+var age = 23;
+console.log(age >=15  &&  age<=65);
+
+var var1 = 5;
+var var2= 6;
+console.log(var1 == 5 || var2 == 5);
