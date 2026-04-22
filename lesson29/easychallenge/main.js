4e9d298d841f174acbeb6406b2a6faa1738cdf0a
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-04-22 16:08:55 +0200

    commit

---
+++ lesson29/easychallenge/main.js
@@ -0,0 +1,10 @@
+var students = ['Rita','Festina', 'Mjellma','Valza','Diella'];
+
+console.log(students);
+
+var[s1,s2,s3,s4,s5] = students;
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(s5);
