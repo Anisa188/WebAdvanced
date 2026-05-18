34d1f4fbdb43e06fbc5c0d9487361918f7df5185
Anisa188 <anisathaqi85@gmail.com>
2026-05-18 16:14:29 +0200

    commit

--- lesson33/main.js
+++ lesson33/main.js
@@ -14,33 +14,45 @@ function validation(){
 
         if(!(name.match(valid_age_regex))){
             document.getElementById("name_error").style.visibility = "visible";
-            document.getElementById("name").style.borderColor = "red"
+            document.getElementById("name").style.borderColor = "red";
 
         }else{
             
             document.getElementById("name_error").style.visibility = "hidden";
-            document.getElementById("name").style.borderColor = "black"
+            document.getElementById("name").style.borderColor = "black";
         }
 
          if(!(age.match(valid_age_regex))){
             document.getElementById("age_error").style.visibility = "visible";
-            document.getElementById("age").style.borderColor = "red"
+            document.getElementById("age").style.borderColor = "red";
 
         }else{
             
             document.getElementById("age_error").style.visibility = "hidden";
-            document.getElementById("age").style.borderColor = "black"
+            document.getElementById("age").style.borderColor = "black";
         }
 
          if(city == ""){
-            document.getElementById("name_error").style.visibility = "visible";
-            document.getElementById("name").style.borderColor = "red"
+            document.getElementById("city_error").style.visibility = "visible";
+            document.getElementById("city").style.borderColor = "red";
+
+        }else{
+            
+            document.getElementById("city_error").style.visibility = "hidden";
+            document.getElementById("city").style.borderColor = "black";
+        }
+        return false;
 
         }else{
             
             document.getElementById("name_error").style.visibility = "hidden";
             document.getElementById("name").style.borderColor = "black"
-        }
-        return false;
+            document.getElementById("age_error").style.visibility = "hidden";
+            document.getElementById("age").style.borderColor = "black"
+            document.getElementById("city_error").style.visibility = "hidden";
+            document.getElementById("city").style.borderColor = "black"
+            return true;
+ 
     }
 }
+
